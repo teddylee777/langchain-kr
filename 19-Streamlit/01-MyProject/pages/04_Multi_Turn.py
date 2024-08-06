7ea2e2edb0cb38f913b14f708dd9f47e1999a0fb
teddy <teddylee777@gmail.com>
2024-08-06 22:32:45 +0900

    update tutorial

--- 19-Streamlit/01-MyProject/pages/04_Multi_Turn.py
+++ 19-Streamlit/01-MyProject/pages/04_Multi_Turn.py
@@ -1,40 +1,58 @@
+from urllib import response
+from requests import session
 import streamlit as st
 from langchain_core.messages.chat import ChatMessage
 from langchain_openai import ChatOpenAI
-from langchain_core.output_parsers import StrOutputParser
+from langchain_teddynote import logging
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_teddynote import logging
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+
 from dotenv import load_dotenv
+import os
 
 
 # API KEY 정보로드
 load_dotenv()
 
 # 프로젝트 이름을 입력합니다.
-logging.langsmith("[Project] Multi-Turn Chat")
+logging.langsmith("[Project] Multi Tuen 챗봇")
 
-st.title("멀티턴 챗봇 💬")
+# 캐시 디렉토리 생성
+if not os.path.exists(".cache"):
+    os.mkdir(".cache")
+
+# 파일 업로드 전용 폴더
+if not os.path.exists(".cache/files"):
+    os.mkdir(".cache/files")
+
+if not os.path.exists(".cache/embeddings"):
+    os.mkdir(".cache/embeddings")
+
+st.title("대화내용을 기억하는 챗봇 💬")
 
 # 처음 1번만 실행하기 위한 코드
 if "messages" not in st.session_state:
     # 대화기록을 저장하기 위한 용도로 생성한다.
     st.session_state["messages"] = []
 
-# Chain 저장용
-if "chain" not in st.session_state:
-    # 아무런 파일을 업로드 하지 않을 경우
-    st.session_state["chain"] = None
-
-# 대화 내용을 기억하기 위한 저장소 생성
 if "store" not in st.session_state:
     st.session_state["store"] = {}
+
 
 # 사이드바 생성
 with st.sidebar:
     # 초기화 버튼 생성
     clear_btn = st.button("대화 초기화")
+
+    # 모델 선택 메뉴
+    selected_model = st.selectbox("LLM 선택", ["gpt-4o", "gpt-4o-mini"], index=0)
+
+    # 세션 ID 를 지정하는 메뉴
+    session_id = st.text_input("세션 ID를 입력하세요.", "abc123")
 
 
 # 이전 대화를 출력
@@ -57,9 +75,8 @@ def get_session_history(session_ids):
 
 
 # 체인 생성
-def create_chain():
-    # 단계 6: 프롬프트 생성(Create Prompt)
-    # 프롬프트를 생성합니다.
+def create_chain(model_name="gpt-4o"):
+
     # 프롬프트 정의
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -74,9 +91,9 @@ def create_chain():
     )
 
     # llm 생성
-    llm = ChatOpenAI()
+    llm = ChatOpenAI(model_name="gpt-4o")
 
-    # 단계 8: 체인(Chain) 생성
+    # 일반 Chain 생성
     chain = prompt | llm | StrOutputParser()
 
     chain_with_history = RunnableWithMessageHistory(
@@ -101,20 +118,24 @@ user_input = st.chat_input("궁금한 내용을 물어보세요!")
 # 경고 메시지를 띄우기 위한 빈 영역
 warning_msg = st.empty()
 
-if st.session_state["chain"] is None:
-    st.session_state["chain"] = create_chain()
+if "chain" not in st.session_state:
+    st.session_state["chain"] = create_chain(model_name=selected_model)
+
 
 # 만약에 사용자 입력이 들어오면...
 if user_input:
-    # chain 을 생성
     chain = st.session_state["chain"]
-
     if chain is not None:
+        response = chain.stream(
+            # 질문 입력
+            {"question": user_input},
+            # 세션 ID 기준으로 대화를 기록합니다.
+            config={"configurable": {"session_id": session_id}},
+        )
+
         # 사용자의 입력
         st.chat_message("user").write(user_input)
-        # 스트리밍 호출
-        config = {"configurable": {"session_id": "abc123"}}
-        response = chain.stream({"question": user_input}, config=config)
+
         with st.chat_message("assistant"):
             # 빈 공간(컨테이너)을 만들어서, 여기에 토큰을 스트리밍 출력한다.
             container = st.empty()
@@ -124,6 +145,9 @@ if user_input:
                 ai_answer += token
                 container.markdown(ai_answer)
 
-        # 대화기록을 저장한다.
-        add_message("user", user_input)
-        add_message("assistant", ai_answer)
+            # 대화기록을 저장한다.
+            add_message("user", user_input)
+            add_message("assistant", ai_answer)
+    else:
+        # 이미지를 업로드 하라는 경고 메시지 출력
+        warning_msg.error("이미지를 업로드 해주세요.")
